5a8e375a8cd7f9ffe3fea9d15a96f05ee4f238bc
Xavier Stouder <xavier@stouder.io>
2023-05-17 18:04:44 +0200

    fix: split state content and dispatch function

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,11 +1,7 @@
 import { useContext } from 'react';
 
 import { DataContext } from '../components/context/DataContext';
-import { DispatchContext } from '../components/context/DispatchContext';
 
 export default function useData() {
-  return {
-    data: useContext(DataContext),
-    dispatch: useContext(DispatchContext).data,
-  };
+  return useContext(DataContext);
 }
