3cbe83392dd3f381b03c802bd609f245813286d6
Xavier Stouder <xavier@stouder.io>
2023-05-18 13:56:39 +0200

    feat: basic header with fullscreen capability

--- src/components/Pixelium.tsx
+++ src/components/Pixelium.tsx
@@ -1,4 +1,8 @@
-import { memo, useEffect, useMemo, useReducer } from 'react';
+import { css } from '@emotion/react';
+import { memo, useEffect, useMemo, useReducer, useRef } from 'react';
+import { FaRegWindowMaximize, FaWrench, MdSettings } from 'react-icons/all';
+import { DropZone, Header, RootLayout, Toolbar } from 'react-science/ui';
+import { useFullscreen, useToggle } from 'react-use';
 
 import {
   dataReducer,
@@ -16,19 +20,27 @@ import {
   ViewState,
 } from '../state/view/ViewReducer';
 
-import Counter from './Counter';
 import { DataProvider } from './context/DataContext';
 import { DispatchProvider } from './context/DispatchContext';
 import { PreferencesProvider } from './context/PreferencesContext';
 import { ViewProvider } from './context/ViewContext';
 
 interface PixeliumProps {
-  data: DataState;
-  preferences: PreferencesState;
-  view: ViewState;
+  data?: DataState;
+  preferences?: PreferencesState;
+  view?: ViewState;
 }
 
+const pixeliumContainerStyle = css`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`;
+
 function Pixelium({ data, preferences, view }: PixeliumProps) {
+  // Refs
+  const rootRef = useRef<HTMLDivElement>(null);
+
   // Populate contexts
   const [dataState, dispatchData] = useReducer(dataReducer, initialDataState);
   const [preferencesState, dispatchPreferences] = useReducer(
@@ -45,16 +57,40 @@ function Pixelium({ data, preferences, view }: PixeliumProps) {
     };
   }, [dispatchData, dispatchPreferences, dispatchView]);
 
+  const [show, toggle] = useToggle(false);
+
+  const isFullScreen = useFullscreen(rootRef, show, {
+    onClose: () => toggle(false),
+  });
+
   return (
-    <DataProvider value={dataState}>
-      <PreferencesProvider value={preferencesState}>
-        <ViewProvider value={viewState}>
-          <DispatchProvider value={dispatchers}>
-            <Counter />
-          </DispatchProvider>
-        </ViewProvider>
-      </PreferencesProvider>
-    </DataProvider>
+    <RootLayout>
+      <DataProvider value={dataState}>
+        <PreferencesProvider value={preferencesState}>
+          <ViewProvider value={viewState}>
+            <DispatchProvider value={dispatchers}>
+              <div css={pixeliumContainerStyle} ref={rootRef}>
+                <Header>
+                  <Toolbar orientation="horizontal">
+                    <span>Pixelium</span>
+                  </Toolbar>
+                  <Toolbar orientation="horizontal">
+                    <Toolbar.Item title="Settings">
+                      <FaWrench />
+                    </Toolbar.Item>
+                    {!isFullScreen && (
+                      <Toolbar.Item title="Full Screen" onClick={toggle}>
+                        <FaRegWindowMaximize />
+                      </Toolbar.Item>
+                    )}
+                  </Toolbar>
+                </Header>
+              </div>
+            </DispatchProvider>
+          </ViewProvider>
+        </PreferencesProvider>
+      </DataProvider>
+    </RootLayout>
   );
 }
 
