a089c0bcbd64c0bacd95990837dda4b38b4ee37e
Xavier Stouder <xavier@stouder.io>
2023-06-05 11:45:02 +0200

    fix: correctly log error on image import

--- src/hooks/useFileLoader.ts
+++ src/hooks/useFileLoader.ts
@@ -1,4 +1,4 @@
-import { fileCollectionFromFiles } from 'filelist-utils';
+import { fileCollectionFromFiles, FileCollectionItem } from 'filelist-utils';
 import { decode } from 'image-js';
 import { useCallback } from 'react';
 
@@ -6,41 +6,57 @@ import { LOAD_DROP, SET_LOADING } from '../state/data/DataActionTypes';
 import { ImageWithMetadata } from '../state/data/DataReducer';
 
 import useDataDispatch from './useDataDispatch';
+import useLog from './useLog';
 
 export default function useFileLoader() {
   const dataDispatch = useDataDispatch();
 
+  const { logger } = useLog();
+
   const loadFiles = useCallback(
     async (files: File[]) => {
-      // TODO handle errors
-      const fileCollection = await fileCollectionFromFiles(files);
+      const fileCollection = await fileCollectionFromFiles(files)
+        .then((fileCollection) => fileCollection.files)
+        .catch(() => {
+          logger.error('Error loading files');
+          return [] as FileCollectionItem[];
+        });
       const imagesWithMetadata: ImageWithMetadata[] = [];
 
       // for each image, decode it
-      for (const file of fileCollection.files) {
-        const image = decode(new Uint8Array(await file.arrayBuffer()));
-        const metadata = {
-          name: file.name,
-          relativePath: file.relativePath,
-        };
-        imagesWithMetadata.push({
-          image,
-          metadata,
-        });
+      for (const file of fileCollection) {
+        try {
+          const image = decode(new Uint8Array(await file.arrayBuffer()));
+          const metadata = {
+            name: file.name,
+            relativePath: file.relativePath,
+          };
+          imagesWithMetadata.push({
+            image,
+            metadata,
+          });
+        } catch {
+          logger.error(`Error decoding file ${file.name}`);
+        }
       }
       dataDispatch({ type: LOAD_DROP, payload: imagesWithMetadata });
+      return imagesWithMetadata.length;
     },
-    [dataDispatch],
+    [dataDispatch, logger],
   );
 
   const handleFileLoad = useCallback(
     (acceptedFiles) => {
       dataDispatch({ type: SET_LOADING, payload: true });
-      loadFiles(acceptedFiles).finally(() =>
-        dataDispatch({ type: SET_LOADING, payload: false }),
-      );
+      loadFiles(acceptedFiles)
+        .then((count) =>
+          logger.info(
+            count === 0 ? 'No image loaded' : `Loaded ${count} images`,
+          ),
+        )
+        .finally(() => dataDispatch({ type: SET_LOADING, payload: false }));
     },
-    [dataDispatch, loadFiles],
+    [dataDispatch, loadFiles, logger],
   );
 
   return handleFileLoad;
