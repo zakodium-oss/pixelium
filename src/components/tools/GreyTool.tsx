8e44f7a3bf11bd36274bb2ddca63860d4abde5d4
Xavier Stouder <xavier@stouder.io>
2023-06-08 11:48:07 +0200

    fix: hide grey tool on already grey images

--- src/components/tools/GreyTool.tsx
+++ src/components/tools/GreyTool.tsx
@@ -1,14 +1,17 @@
 import { FaFilter } from 'react-icons/all';
 import { Toolbar, useOnOff } from 'react-science/ui';
 
+import useData from '../../hooks/useData';
 import useView from '../../hooks/useView';
 import ExploreGreyModal from '../modal/ExploreGreyModal';
 
 export function GreyTool() {
   const [isOpenDialog, openDialog, closeDialog] = useOnOff(false);
   const { currentTab } = useView();
+  const { files } = useData();
 
   if (currentTab === undefined) return null;
+  if (!files[currentTab].image.colorModel.includes('RGB')) return null;
 
   return (
     <>
