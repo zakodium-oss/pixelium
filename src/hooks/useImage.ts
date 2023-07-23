4d7eec549f2c60e9b7a26c953432379a459e1594
Xavier Stouder <xavier@stouder.io>
2023-07-23 18:09:50 +0200

    fix: better memoization

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -1,14 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 
 import { PipelineContext } from '../components/context/PipelineContext';
 
 export default function useImage(toOperation?: string) {
   const pipelineContent = useContext(PipelineContext);
 
-  const toReturn = {
-    original: pipelineContent.original,
-    pipelined: pipelineContent.pipelined(toOperation),
-  };
-
-  return toReturn;
+  return useMemo(
+    () => ({
+      original: pipelineContent.original,
+      pipelined: pipelineContent.pipelined(toOperation),
+    }),
+    [pipelineContent, toOperation],
+  );
 }
