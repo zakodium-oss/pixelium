66763fb53dd6106be7009ede24a31f9906f27216
Xavier Stouder <xavier@stouder.io>
2023-07-27 14:04:32 +0200

    chore: populate about modal

--- src/components/modal/AboutModal.tsx
+++ src/components/modal/AboutModal.tsx
@@ -1,7 +1,10 @@
 import styled from '@emotion/styled';
 import { memo } from 'react';
+import { FaFileSignature, FaGithub } from 'react-icons/fa';
 import { Modal } from 'react-science/ui';
 
+import hearcLogo from '../../assets/hearc-logo.png';
+import zakodiumLogo from '../../assets/zakodium-logo.png';
 import useModal from '../../hooks/useModal';
 
 import StyledModalBody from './utils/StyledModalBody';
@@ -17,6 +20,62 @@ const AboutModalStyle = styled.div`
   }
 `;
 
+const AboutContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  h2 {
+    margin-bottom: 10px;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  p {
+    margin-bottom: 10px;
+    font-size: 1rem;
+    text-align: center;
+  }
+`;
+
+const ImageColumns = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  margin-bottom: 10px;
+
+  a {
+    width: 50%;
+  }
+`;
+
+const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  font-size: 1rem;
+
+  a {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+
 function AboutModal() {
   const { isOpen, close } = useModal('about');
 
@@ -27,8 +86,51 @@ function AboutModal() {
           <Modal.Header>About Pixelium</Modal.Header>
         </StyledModalHeader>
         <StyledModalBody>
-          <Modal.Body>Nothing to see here at this time.</Modal.Body>
+          <Modal.Body>
+            <AboutContent>
+              <h2>Pixelium</h2>
+              <p>
+                Pixelium is a React component for displaying and processing
+                raster images.
+              </p>
+              <ImageColumns>
+                <a
+                  href="https://zakodium.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={zakodiumLogo} />
+                </a>
+                <a
+                  href="https://www.he-arc.ch/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={hearcLogo} />
+                </a>
+              </ImageColumns>
+              <p>All rights reserved. Released under MIT license.</p>
+            </AboutContent>
+          </Modal.Body>
         </StyledModalBody>
+        <Modal.Footer>
+          <LinksContainer>
+            <a
+              href="https://github.com/zakodium-oss/pixelium"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub /> Source code
+            </a>
+            <a
+              href="https://github.com/zakodium-oss/pixelium/blob/main/LICENSE"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFileSignature /> License
+            </a>
+          </LinksContainer>
+        </Modal.Footer>
       </AboutModalStyle>
     </Modal>
   );
