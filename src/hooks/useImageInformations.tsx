76a1cde2b3c62326395d469aba22013fd626fdce
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2023-08-17 16:38:43 +0100

    refactor: impove information panel

--- src/hooks/useImageInformations.tsx
+++ src/hooks/useImageInformations.tsx
@@ -1,37 +1,19 @@
 import { Image } from 'image-js';
 import { useMemo } from 'react';
-import { ValueRenderers } from 'react-science/ui';
 
 export default function useImageInformations(image: Image | null) {
   return useMemo(() => {
-    if (image === null) return [];
-    return [
-      {
-        key: 'Image size',
-        render: (
-          <ValueRenderers.Text
-            value={`${image.width.toLocaleString()} x ${image.height.toLocaleString()} (${(
-              image.width * image.height
-            ).toLocaleString()} pixels)`}
-          />
-        ),
-      },
-      {
-        key: 'Bit depth',
-        render: <ValueRenderers.Number value={image.bitDepth} />,
-      },
-      {
-        key: 'Channels',
-        render: <ValueRenderers.Number value={image.channels} />,
-      },
-      {
-        key: 'Components',
-        render: <ValueRenderers.Number value={image.components} />,
-      },
-      {
-        key: 'Color model',
-        render: <ValueRenderers.Text value={image.colorModel} />,
-      },
-    ];
+    if (image === null) return { info: {}, meta: {} };
+    const info = {
+      imageSize: `${image.width.toLocaleString()} x ${image.height.toLocaleString()} (${(
+        image.width * image.height
+      ).toLocaleString()} pixels)`,
+      bitDepth: image.bitDepth,
+      channels: image.channels,
+      components: image.components,
+      colorModel: image.colorModel,
+    };
+    const meta = image.meta?.tiff.tags || {};
+    return { info, meta };
   }, [image]);
 }
