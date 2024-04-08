d7ff0e0bba7b61f7f36c28305fb8c054d8ff9b6a
Xavier Stouder <xavier@stouder.io>
2024-04-08 10:00:36 +0000

    Updates

--- assets/allPaths-1ef0960f.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-0aa86537.js";import{I as e}from"./index-4387c2ca.js";import{p as r,I as s}from"./index-803d16af.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
