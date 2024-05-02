32c899de58e073716ca14ca83e7242ab4a4a1944
moonayyur <jm_kias@esi.dz>
2024-05-02 11:39:15 +0200

    feat: color histogram on selected interval

--- src/components/roi/ROIFilter.tsx
+++ src/components/roi/ROIFilter.tsx
@@ -22,7 +22,7 @@ function ROIFilter({
   column: Column<RoiDataType, unknown>;
   table: Table<RoiDataType>;
 }) {
-  const columnFilterValue = column.getFilterValue();
+  const columnFilterValue = column.getFilterValue() as [number, number];
 
   // Create a new table without the current column filter for the histogram
   const histTable = useReactTable({
@@ -76,12 +76,15 @@ function ROIFilter({
           padding: 10,
         }}
       >
-        <Histogram values={histValues} />
-        <div style={{ width: '100%', paddingLeft: 30 }}>
+        <Histogram
+          values={histValues}
+          columnFilterValue={columnFilterValue ?? [min, max]}
+        />
+        <div style={{ width: '100%', paddingLeft: 18 }}>
           <RangeSlider
             min={min}
             max={max}
-            value={(columnFilterValue as [number, number]) ?? [min, max]}
+            value={columnFilterValue ?? [min, max]}
             stepSize={
               ['id', 'column', 'row', 'width', 'height', 'surface'].includes(
                 column.id,
@@ -108,11 +111,11 @@ function ROIFilter({
             type="number"
             min={min ?? ''}
             max={max ?? ''}
-            value={Number((columnFilterValue as [number, number])?.[0]) || ''}
+            value={Number(columnFilterValue?.[0]) || ''}
             onChange={(value) =>
               column.setFilterValue((old: [number, number]) => [
-                value,
-                old?.[1],
+                Number(value),
+                old?.[1] || max,
               ])
             }
             placeholder={`${min ?? ''}`}
@@ -123,11 +126,11 @@ function ROIFilter({
             type="number"
             min={min ?? ''}
             max={max ?? ''}
-            value={Number((columnFilterValue as [number, number])?.[1]) || ''}
+            value={Number(columnFilterValue?.[1]) || ''}
             onChange={(value) =>
               column.setFilterValue((old: [number, number]) => [
-                old?.[0],
-                value,
+                old?.[0] || min,
+                Number(value),
               ])
             }
             placeholder={`${max ?? ''}`}
@@ -141,7 +144,13 @@ function ROIFilter({
   );
 }
 
-function Histogram({ values }: { values: number[] }) {
+function Histogram({
+  values,
+  columnFilterValue,
+}: {
+  values: number[];
+  columnFilterValue: [number, number];
+}) {
   const histogram =
     values.length > 0
       ? xHistogram(values, {
@@ -155,9 +164,17 @@ function Histogram({ values }: { values: number[] }) {
     y: point.y,
   }));
 
+  const activeHistData = useMemo(() => {
+    return histData.filter(
+      (point) =>
+        point.x > columnFilterValue[0] && point.x < columnFilterValue[1],
+    );
+  }, [histData, columnFilterValue]);
+
   return (
     <Plot width={200} height={100}>
       <BarSeries data={histData} lineStyle={{ stroke: 'cornflowerblue' }} />
+      <BarSeries data={activeHistData} lineStyle={{ stroke: 'orange' }} />
       <Axis min={0} position="left" hiddenLine />
       <Axis position="bottom" hiddenTicks />
     </Plot>
