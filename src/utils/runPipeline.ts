fe20d421a6978f76e05d0a37174fdf895c96459c
moonayyur <jm_kias@esi.dz>
2024-05-27 23:41:41 +0200

    refactor: compute pipeline time in the generic part of the loop

--- src/utils/runPipeline.ts
+++ src/utils/runPipeline.ts
@@ -22,124 +22,77 @@ export default function runPipeline(
     const applyOn = pipelineSteps[index - 1]?.result ?? baseImage;
     if (applyOn === undefined) break;
 
+    let result = applyOn;
+    const time0 = performance.now();
+
     try {
       switch (operation.type) {
         case 'GREY_FILTER': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.grey({
+            result = applyOn.grey({
               algorithm: operation.options.algorithm,
-            });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
             });
           }
           break;
         }
         case 'CONVERT': {
-          if (applyOn instanceof Image) {
-            let result = applyOn;
-            const time0 = performance.now();
+          if (applyOn instanceof Image && result instanceof Image) {
             if (operation.options.colorModel !== applyOn.colorModel) {
               result = result.convertColor(operation.options.colorModel);
             }
             if (operation.options.bitDepth !== applyOn.bitDepth) {
               result = result.convertBitDepth(operation.options.bitDepth);
             }
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
-            });
           }
           break;
         }
         case 'BLUR': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.blur({
+            result = applyOn.blur({
               width: operation.options.width,
               height: operation.options.height,
               borderType: operation.options.borderType,
               borderValue: operation.options.borderValue,
-            });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
             });
           }
           break;
         }
         case 'MASK': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.threshold({
+            result = applyOn.threshold({
               algorithm: operation.options.algorithm,
               slots: operation.options.slots,
-            });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
             });
           }
           break;
         }
         case 'GAUSSIAN_BLUR': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.gaussianBlur({
+            result = applyOn.gaussianBlur({
               sigmaX: operation.options.sigmaX,
               sigmaY: operation.options.sigmaY,
               sizeX: operation.options.sizeX,
               sizeY: operation.options.sizeY,
             });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
-            });
           }
           break;
         }
         case 'INVERT': {
-          const time0 = performance.now();
-          const result = applyOn.invert();
-          const time = performance.now() - time0;
-          pipelineSteps.push({
-            identifier: operation.identifier,
-            result,
-            time,
-          });
+          result = applyOn.invert();
+
           break;
         }
         case 'FLIP': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.flip({
+            result = applyOn.flip({
               axis: operation.options.axis,
-            });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
             });
           }
           break;
         }
         case 'LEVEL': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.level({
+            result = applyOn.level({
               channels: operation.options.channels,
               inputMin: operation.options.inputMin,
               inputMax: operation.options.inputMax,
@@ -147,108 +100,59 @@ export default function runPipeline(
               outputMax: operation.options.outputMax,
               gamma: operation.options.gamma,
             });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
-            });
           }
           break;
         }
         case 'PIXELATE': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.pixelate({
+            result = applyOn.pixelate({
               cellSize: operation.options.cellSize,
               algorithm: operation.options.algorithm,
-            });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
             });
           }
           break;
         }
         case 'MEDIAN_FILTER': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.medianFilter({
+            result = applyOn.medianFilter({
               cellSize: operation.options.cellSize,
               borderType: operation.options.borderType,
               borderValue: operation.options.borderValue,
-            });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
             });
           }
           break;
         }
         case 'DILATE': {
-          const time0 = performance.now();
-          const result = applyOn.dilate({
+          result = applyOn.dilate({
             kernel: operation.options.kernel,
             iterations: operation.options.iterations,
-          });
-          const time = performance.now() - time0;
-          pipelineSteps.push({
-            identifier: operation.identifier,
-            result,
-            time,
           });
           break;
         }
         case 'ERODE': {
-          const time0 = performance.now();
-          const result = applyOn.erode({
+          result = applyOn.erode({
             kernel: operation.options.kernel,
             iterations: operation.options.iterations,
-          });
-          const time = performance.now() - time0;
-          pipelineSteps.push({
-            identifier: operation.identifier,
-            result,
-            time,
           });
           break;
         }
         case 'OPEN': {
-          const time0 = performance.now();
-          const result = applyOn.open({
+          result = applyOn.open({
             kernel: operation.options.kernel,
             iterations: operation.options.iterations,
-          });
-          const time = performance.now() - time0;
-          pipelineSteps.push({
-            identifier: operation.identifier,
-            result,
-            time,
           });
           break;
         }
         case 'CLOSE': {
-          const time0 = performance.now();
-          const result = applyOn.close({
+          result = applyOn.close({
             kernel: operation.options.kernel,
             iterations: operation.options.iterations,
-          });
-          const time = performance.now() - time0;
-          pipelineSteps.push({
-            identifier: operation.identifier,
-            result,
-            time,
           });
           break;
         }
         case 'RESIZE': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.resize({
+            result = applyOn.resize({
               width: operation.options.width,
               height: operation.options.height,
               interpolationType: operation.options.interpolationType,
@@ -256,28 +160,15 @@ export default function runPipeline(
               preserveAspectRatio: operation.options.preserveAspectRatio,
               borderType: operation.options.borderType,
             });
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
-            });
           }
           break;
         }
         case 'ROTATE': {
           if (applyOn instanceof Image) {
-            const time0 = performance.now();
-            const result = applyOn.rotate(
+            result = applyOn.rotate(
               (operation.options.angle *
                 (operation.options.clockwise ? 1 : -1)) as RotateAngle,
             );
-            const time = performance.now() - time0;
-            pipelineSteps.push({
-              identifier: operation.identifier,
-              result,
-              time,
-            });
           }
           break;
         }
@@ -288,6 +179,13 @@ export default function runPipeline(
       pipelineError = error.message;
       break;
     }
+
+    const time = performance.now() - time0;
+    pipelineSteps.push({
+      identifier: operation.identifier,
+      result,
+      time,
+    });
   }
 
   return { pipelineSteps, pipelineError };
