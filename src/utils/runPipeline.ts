ab06c622869c67941a64503966f31381b58de4ec
moonayyur <jm_kias@esi.dz>
2024-05-29 15:48:50 +0200

    refactor: put switch operation cases in separate function

--- src/utils/runPipeline.ts
+++ src/utils/runPipeline.ts
@@ -8,173 +8,176 @@ export interface PipelineStep {
   time: number;
 }
 
+function runPipelineOperation(
+  operation: PipelineOperations,
+  applyOn: Image | Mask,
+) {
+  switch (operation.type) {
+    case 'GREY_FILTER': {
+      if (applyOn instanceof Image) {
+        return applyOn.grey({
+          algorithm: operation.options.algorithm,
+        });
+      }
+      break;
+    }
+    case 'CONVERT': {
+      if (applyOn instanceof Image) {
+        if (operation.options.colorModel !== applyOn.colorModel) {
+          applyOn = applyOn.convertColor(operation.options.colorModel);
+        }
+        if (operation.options.bitDepth !== applyOn.bitDepth) {
+          applyOn = applyOn.convertBitDepth(operation.options.bitDepth);
+        }
+        return applyOn;
+      }
+      break;
+    }
+    case 'BLUR': {
+      if (applyOn instanceof Image) {
+        return applyOn.blur({
+          width: operation.options.width,
+          height: operation.options.height,
+          borderType: operation.options.borderType,
+          borderValue: operation.options.borderValue,
+        });
+      }
+      break;
+    }
+    case 'MASK': {
+      if (applyOn instanceof Image) {
+        return applyOn.threshold({
+          algorithm: operation.options.algorithm,
+          slots: operation.options.slots,
+        });
+      }
+      break;
+    }
+    case 'GAUSSIAN_BLUR': {
+      if (applyOn instanceof Image) {
+        return applyOn.gaussianBlur({
+          sigmaX: operation.options.sigmaX,
+          sigmaY: operation.options.sigmaY,
+          sizeX: operation.options.sizeX,
+          sizeY: operation.options.sizeY,
+        });
+      }
+      break;
+    }
+    case 'INVERT': {
+      return applyOn.invert();
+    }
+    case 'FLIP': {
+      if (applyOn instanceof Image) {
+        return applyOn.flip({
+          axis: operation.options.axis,
+        });
+      }
+      break;
+    }
+    case 'LEVEL': {
+      if (applyOn instanceof Image) {
+        return applyOn.level({
+          channels: operation.options.channels,
+          inputMin: operation.options.inputMin,
+          inputMax: operation.options.inputMax,
+          outputMin: operation.options.outputMin,
+          outputMax: operation.options.outputMax,
+          gamma: operation.options.gamma,
+        });
+      }
+      break;
+    }
+    case 'PIXELATE': {
+      if (applyOn instanceof Image) {
+        return applyOn.pixelate({
+          cellSize: operation.options.cellSize,
+          algorithm: operation.options.algorithm,
+        });
+      }
+      break;
+    }
+    case 'MEDIAN_FILTER': {
+      if (applyOn instanceof Image) {
+        return applyOn.medianFilter({
+          cellSize: operation.options.cellSize,
+          borderType: operation.options.borderType,
+          borderValue: operation.options.borderValue,
+        });
+      }
+      break;
+    }
+    case 'DILATE': {
+      return applyOn.dilate({
+        kernel: operation.options.kernel,
+        iterations: operation.options.iterations,
+      });
+    }
+    case 'ERODE': {
+      return applyOn.erode({
+        kernel: operation.options.kernel,
+        iterations: operation.options.iterations,
+      });
+    }
+    case 'OPEN': {
+      return applyOn.open({
+        kernel: operation.options.kernel,
+        iterations: operation.options.iterations,
+      });
+    }
+    case 'CLOSE': {
+      return applyOn.close({
+        kernel: operation.options.kernel,
+        iterations: operation.options.iterations,
+      });
+    }
+    case 'RESIZE': {
+      if (applyOn instanceof Image) {
+        return applyOn.resize({
+          width: operation.options.width,
+          height: operation.options.height,
+          interpolationType: operation.options.interpolationType,
+          borderValue: operation.options.borderValue,
+          preserveAspectRatio: operation.options.preserveAspectRatio,
+          borderType: operation.options.borderType,
+        });
+      }
+      break;
+    }
+    case 'ROTATE': {
+      if (applyOn instanceof Image) {
+        return applyOn.rotate(
+          (operation.options.angle *
+            (operation.options.clockwise ? 1 : -1)) as RotateAngle,
+        );
+      }
+      break;
+    }
+    default:
+      throw new Error(`Unknown operation type ${(operation as any).type}`);
+  }
+}
+
 export default function runPipeline(
   pipeline: PipelineOperations[],
   baseImage: Image,
 ) {
   const pipelineSteps: PipelineStep[] = [];
   let pipelineError: string | undefined;
-  for (const [index, operation] of pipeline.entries()) {
+
+  let result: Image | Mask = baseImage;
+
+  for (const [, operation] of pipeline.entries()) {
     if (!operation.isActive) {
       break;
     }
 
-    const applyOn = pipelineSteps[index - 1]?.result ?? baseImage;
-    if (applyOn === undefined) break;
+    if (result === undefined) break;
 
-    let result = applyOn;
     const time0 = performance.now();
 
     try {
-      switch (operation.type) {
-        case 'GREY_FILTER': {
-          if (applyOn instanceof Image) {
-            result = applyOn.grey({
-              algorithm: operation.options.algorithm,
-            });
-          }
-          break;
-        }
-        case 'CONVERT': {
-          if (applyOn instanceof Image && result instanceof Image) {
-            if (operation.options.colorModel !== applyOn.colorModel) {
-              result = result.convertColor(operation.options.colorModel);
-            }
-            if (operation.options.bitDepth !== applyOn.bitDepth) {
-              result = result.convertBitDepth(operation.options.bitDepth);
-            }
-          }
-          break;
-        }
-        case 'BLUR': {
-          if (applyOn instanceof Image) {
-            result = applyOn.blur({
-              width: operation.options.width,
-              height: operation.options.height,
-              borderType: operation.options.borderType,
-              borderValue: operation.options.borderValue,
-            });
-          }
-          break;
-        }
-        case 'MASK': {
-          if (applyOn instanceof Image) {
-            result = applyOn.threshold({
-              algorithm: operation.options.algorithm,
-              slots: operation.options.slots,
-            });
-          }
-          break;
-        }
-        case 'GAUSSIAN_BLUR': {
-          if (applyOn instanceof Image) {
-            result = applyOn.gaussianBlur({
-              sigmaX: operation.options.sigmaX,
-              sigmaY: operation.options.sigmaY,
-              sizeX: operation.options.sizeX,
-              sizeY: operation.options.sizeY,
-            });
-          }
-          break;
-        }
-        case 'INVERT': {
-          result = applyOn.invert();
-
-          break;
-        }
-        case 'FLIP': {
-          if (applyOn instanceof Image) {
-            result = applyOn.flip({
-              axis: operation.options.axis,
-            });
-          }
-          break;
-        }
-        case 'LEVEL': {
-          if (applyOn instanceof Image) {
-            result = applyOn.level({
-              channels: operation.options.channels,
-              inputMin: operation.options.inputMin,
-              inputMax: operation.options.inputMax,
-              outputMin: operation.options.outputMin,
-              outputMax: operation.options.outputMax,
-              gamma: operation.options.gamma,
-            });
-          }
-          break;
-        }
-        case 'PIXELATE': {
-          if (applyOn instanceof Image) {
-            result = applyOn.pixelate({
-              cellSize: operation.options.cellSize,
-              algorithm: operation.options.algorithm,
-            });
-          }
-          break;
-        }
-        case 'MEDIAN_FILTER': {
-          if (applyOn instanceof Image) {
-            result = applyOn.medianFilter({
-              cellSize: operation.options.cellSize,
-              borderType: operation.options.borderType,
-              borderValue: operation.options.borderValue,
-            });
-          }
-          break;
-        }
-        case 'DILATE': {
-          result = applyOn.dilate({
-            kernel: operation.options.kernel,
-            iterations: operation.options.iterations,
-          });
-          break;
-        }
-        case 'ERODE': {
-          result = applyOn.erode({
-            kernel: operation.options.kernel,
-            iterations: operation.options.iterations,
-          });
-          break;
-        }
-        case 'OPEN': {
-          result = applyOn.open({
-            kernel: operation.options.kernel,
-            iterations: operation.options.iterations,
-          });
-          break;
-        }
-        case 'CLOSE': {
-          result = applyOn.close({
-            kernel: operation.options.kernel,
-            iterations: operation.options.iterations,
-          });
-          break;
-        }
-        case 'RESIZE': {
-          if (applyOn instanceof Image) {
-            result = applyOn.resize({
-              width: operation.options.width,
-              height: operation.options.height,
-              interpolationType: operation.options.interpolationType,
-              borderValue: operation.options.borderValue,
-              preserveAspectRatio: operation.options.preserveAspectRatio,
-              borderType: operation.options.borderType,
-            });
-          }
-          break;
-        }
-        case 'ROTATE': {
-          if (applyOn instanceof Image) {
-            result = applyOn.rotate(
-              (operation.options.angle *
-                (operation.options.clockwise ? 1 : -1)) as RotateAngle,
-            );
-          }
-          break;
-        }
-        default:
-          throw new Error(`Unknown operation type ${(operation as any).type}`);
-      }
+      result = runPipelineOperation(operation, result) ?? baseImage;
     } catch (error: any) {
       pipelineError = error.message;
       break;
